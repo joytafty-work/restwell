6f693d6b7df6cc5acb793d0737784c4300a8d5e8
joytafty <joytafty@gmail.com>
2013-11-26 15:14:37 -0500

    add activity dataset

--- run.py
+++ run.py
@@ -119,7 +119,7 @@ def server():
         print calories_temp
 
         from datetime import datetime, timedelta
-        temp = [datum for datum in startTime_temp if datum['value'] > 0]
+        temp = [datum for datum in calories_temp if datum['value'] > 0]
         dateall = dict((t['dateTime'], i) for i, t in enumerate(temp))
         dateall = dateall.keys()
 

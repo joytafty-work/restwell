b0f4d5a63c709d7362256ed120b4e2fe764a50a5
joytafty <joytafty@gmail.com>
2013-11-27 00:44:32 -0500

    debugging activity dataset

--- run.py
+++ run.py
@@ -79,6 +79,8 @@ def server():
         fb = fitbit.Fitbit(os.getenv('FITBIT_KEY'), os.getenv('FITBIT_SECRET'), 
             user_key=flask.session['FITBIT_TOKEN'], user_secret=flask.session['FITBIT_TOKEN_SECRET'])
         calories_temp = fb.time_series('activities/calories', period='max')['activities-calories']
+        caloriesTracker_temp = fb.time_series('activities/tracker/calories', period='max')['activities-tracker-calories']
+        print caloriesTracker_temp
 
         from datetime import datetime, timedelta
         # temp = [datum for datum in calories_temp if float(datum['value']) > 0]
